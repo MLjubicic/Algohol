9964d1a775010b7fdb82ad886c62438c4dc203bf
MWeigert <weigemat@students.zhaw.ch>
2012-04-02 20:00:35 +0200

    Minor GUI changes

---
+++ src/krypto/gui/action/NoSpacesListener.java
@@ -0,0 +1,25 @@
+/** ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *  +      Zuercher Hochschule angewandter Wissenschaften      +
+ *  +                    Software Projekt 2                    +
+ *  +                                                          +
+ *  +        Gruppe 10: Miro Ljubicic & Mathias Weigert        +
+ *  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+package krypto.gui.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Mathias Weigert & Miro Ljubicic
+ * @version 1.0
+ */
+public class NoSpacesListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/krypto/gui/panel/CaesarPanel.java
+++ src/krypto/gui/panel/CaesarPanel.java
@@ -7,9 +7,12 @@
  */
 package krypto.gui.panel;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * @author Mathias Weigert & Miro Ljubicic
@@ -30,6 +33,28 @@ public class CaesarPanel {
 		JTextArea plainTAerea = new JTextArea();
 		plainTAerea.setBounds(10, 35, 200, 100);
 		cPanel.add(plainTAerea);
+		
+		JRadioButton noSpacesBtn = new JRadioButton("no spaces");
+		noSpacesBtn.setBounds(225, 35, 100, 25);
+		cPanel.add(noSpacesBtn);
+		
+		JRadioButton stdSpacesBtn = new JRadioButton("standard");
+		stdSpacesBtn.setBounds(330, 35, 100, 25);
+		stdSpacesBtn.setSelected(true);
+		cPanel.add(stdSpacesBtn);
+		
+		JRadioButton fixSpacesBtn = new JRadioButton("fixed");
+		fixSpacesBtn.setBounds(225, 60, 60, 25);
+		cPanel.add(fixSpacesBtn);
+		
+		JTextField spacesField = new JTextField();
+		spacesField.setBounds(285, 60, 30, 25);
+		cPanel.add(spacesField);
+		
+		ButtonGroup spacesBtnGrp = new ButtonGroup();
+		spacesBtnGrp.add(noSpacesBtn);
+		spacesBtnGrp.add(stdSpacesBtn);
+		spacesBtnGrp.add(fixSpacesBtn);
 		
 		JLabel cipherLabel = new JLabel("Ciphertext");
 		cipherLabel.setBounds(10, 135, 75, 25);
