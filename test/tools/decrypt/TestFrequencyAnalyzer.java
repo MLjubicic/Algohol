d635fc4056b05be5dc8f985832acdb7ce6862a2b
Mathias Weigert <har_lea_quin@gmx.de>
2012-06-09 20:58:31 +0200

    Added test txt with plain text and fixed some bug in TestFrequencyAnalyzer

--- test/tools/decrypt/TestFrequencyAnalyzer.java
+++ test/tools/decrypt/TestFrequencyAnalyzer.java
@@ -8,6 +8,7 @@
 package tools.decrypt;
 
 import java.io.File;
+import java.util.Map;
 
 import krypto.io.LoadTextFile;
 import krypto.tools.decrypt.FrequencyAnalyzer;
@@ -24,11 +25,20 @@ public class TestFrequencyAnalyzer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LoadTextFile ltf = new LoadTextFile();
-		File file = new File("D:\\Daten\\Dropbox\\Java Projekte\\EQ2 Logs\\Test_LOG.txt");
+		File file = new File("C:\\Users\\Bunny\\workspace\\Algohol\\doc\\Test.txt");
 		ltf.loadFile(file);
 		FrequencyAnalyzer fa = new FrequencyAnalyzer();
-		fa.countItems(ltf.getStr().toLowerCase());
+		fa.countItems(ltf.getStr().toUpperCase());
 		fa.calculateFrequency();
+		
+		String abc ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Map <Character, Integer> amountMap = fa.getAmountMap();
+		Map <Character, Double> freqMap = fa.getFrequencyMap();
+		System.out.println(ltf.getStr());
+		for (int i=0; i <= 25; i++) {
+			Character bst = abc.charAt(i);
+			System.out.println(bst + "- Anzahl " + amountMap.get(bst) + " Frequenz: " + freqMap.get(bst));
+		}
 	}
 
 }
