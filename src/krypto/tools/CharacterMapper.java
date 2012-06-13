a7e609305d753746c26d4c094ef707b16a41dfe0
Miro Ljubicic <miro@ljubicic.ch>
2012-06-13 23:52:10 +0200

    Erster Entwurf fuer die Mapper-Klassen

---
+++ src/krypto/tools/CharacterMapper.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package krypto.tools;
+
+import java.util.HashMap;
+
+/**
+ * A basic character mapper to map the determined characters 
+ * from a given input to the most frequent characters of a specific language
+ * @author Miro Ljubicic
+ * @version 0.1
+ */
+public class CharacterMapper {
+
+	
+	public HashMap<Character, Character> getCharacterMap(HashMap<Character, Double> originalMap)
+	{
+		HashMap<Character, Character> modifiedMap = new HashMap<Character,Character>();
+	
+		return modifiedMap;
+	}
+	
+}
---
+++ test/tools/CharacterMapperTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package tools;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import krypto.tools.CharacterMapper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A JUnit 4 test for the CharacterMapper class functionality
+ * @author miro
+ *
+ */
+public class CharacterMapperTest {
+
+	CharacterMapper testmapper;
+	HashMap<Character, Double> originalMap;
+	HashMap<Character, Double> resultFrequencyAnalysis;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		testmapper = new CharacterMapper();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertNotNull(testmapper);
+		testmapper.getCharacterMapping(originalMap, resultFrequencyAnalysis);
+		
+	}
+		
+
+}
